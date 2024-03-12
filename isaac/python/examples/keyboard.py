20c5f88ed4c18e6c9286a680c609923c631d5597
Tyler Barkin <tybarkin@gmail.com>
2024-03-12 14:08:47 -0400

    Got keyboard control of jumpy working.

--- isaac/python/examples/keyboard.py
+++ isaac/python/examples/keyboard.py
@@ -1,13 +1,15 @@
 import pygame
 import numpy as np
+import torch
 
 class Keyboard():
-    def __init__(self):
+    def __init__(self, num_actions=1):
         pygame.quit()
         # Initialize Pygame
         pygame.init()
         # Set up the screen
         screen = pygame.display.set_mode((400, 300))
+        self.num_actions = num_actions
 
     def refresh(self):
         pygame.event.pump()
@@ -16,12 +18,20 @@ class Keyboard():
         pygame.event.pump()
 
         # Check for arrow key presses
-        a = [0.0]
+        a = torch.zeros(self.num_actions)
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            a[0] = 1.0
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_UP]:
             a[0] = -1.0
+        if keys[pygame.K_DOWN]:
+            a[0] = 1.0
+        if keys[pygame.K_w]:
+            a[1] = 1.0
+        if keys[pygame.K_s]:
+            a[1] = -1.0
+        if keys[pygame.K_a]:
+            a[2] = 1.0
+        if keys[pygame.K_d]:
+            a[2] = -1.0
 
         return a
 
