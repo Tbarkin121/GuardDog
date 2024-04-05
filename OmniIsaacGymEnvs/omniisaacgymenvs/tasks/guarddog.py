410087a291b44349e93e02f5fecebc4c6da201ad
Tyler Barkin <tybarkin@gmail.com>
2024-04-05 17:42:16 -0400

    Modeled the guarddog environment on anymal. Training seems to be working. I still need to spend some more time porting over some of the OG code for a better comparison between the two

--- OmniIsaacGymEnvs/omniisaacgymenvs/tasks/guarddog.py
+++ OmniIsaacGymEnvs/omniisaacgymenvs/tasks/guarddog.py
@@ -31,10 +31,12 @@ import math
 
 import numpy as np
 import torch
-from omni.isaac.core.articulations import ArticulationView
 from omni.isaac.core.utils.prims import get_prim_at_path
+from omni.isaac.core.utils.torch.rotations import *
 from omniisaacgymenvs.tasks.base.rl_task import RLTask
 from omniisaacgymenvs.robots.articulations.guarddog import Guarddog
+from omniisaacgymenvs.robots.articulations.views.guarddog_view import GuarddogView
+from omniisaacgymenvs.tasks.utils.usd_utils import set_drive
 
 
 class GuarddogTask(RLTask):
@@ -43,8 +45,8 @@ class GuarddogTask(RLTask):
         self.update_config(sim_config)
         self._max_episode_length = 500
 
-        self._num_observations = 4
-        self._num_actions = 1
+        self._num_observations = 48
+        self._num_actions = 12
 
         RLTask.__init__(self, name, env)
         return
@@ -54,18 +56,58 @@ class GuarddogTask(RLTask):
         self._cfg = sim_config.config
         self._task_cfg = sim_config.task_config
 
+        # normalization
+        self.lin_vel_scale = self._task_cfg["env"]["learn"]["linearVelocityScale"]
+        self.ang_vel_scale = self._task_cfg["env"]["learn"]["angularVelocityScale"]
+        self.dof_pos_scale = self._task_cfg["env"]["learn"]["dofPositionScale"]
+        self.dof_vel_scale = self._task_cfg["env"]["learn"]["dofVelocityScale"]
+        self.action_scale = self._task_cfg["env"]["control"]["actionScale"]
+
+        # reward scales
+        self.rew_scales = {}
+        self.rew_scales["lin_vel_xy"] = self._task_cfg["env"]["learn"]["linearVelocityXYRewardScale"]
+        self.rew_scales["ang_vel_z"] = self._task_cfg["env"]["learn"]["angularVelocityZRewardScale"]
+        self.rew_scales["lin_vel_z"] = self._task_cfg["env"]["learn"]["linearVelocityZRewardScale"]
+        self.rew_scales["joint_acc"] = self._task_cfg["env"]["learn"]["jointAccRewardScale"]
+        self.rew_scales["action_rate"] = self._task_cfg["env"]["learn"]["actionRateRewardScale"]
+        self.rew_scales["cosmetic"] = self._task_cfg["env"]["learn"]["cosmeticRewardScale"]
+
+        # command ranges
+        self.command_x_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["linear_x"]
+        self.command_y_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["linear_y"]
+        self.command_yaw_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["yaw"]
+
+        # base init state
+        pos = self._task_cfg["env"]["baseInitState"]["pos"]
+        rot = self._task_cfg["env"]["baseInitState"]["rot"]
+        v_lin = self._task_cfg["env"]["baseInitState"]["vLinear"]
+        v_ang = self._task_cfg["env"]["baseInitState"]["vAngular"]
+        state = pos + rot + v_lin + v_ang
+
+        self.base_init_state = state
+
+        # default joint positions
+        self.named_default_joint_angles = self._task_cfg["env"]["defaultJointAngles"]
+
+        # other
+        self.dt = 1 / 60
+        self.max_episode_length_s = self._task_cfg["env"]["learn"]["episodeLength_s"]
+        self.max_episode_length = int(self.max_episode_length_s / self.dt + 0.5)
+        self.Kp = self._task_cfg["env"]["control"]["stiffness"]
+        self.Kd = self._task_cfg["env"]["control"]["damping"]
+
+        for key in self.rew_scales.keys():
+            self.rew_scales[key] *= self.dt
+
         self._num_envs = self._task_cfg["env"]["numEnvs"]
+        self._guarddog_translation = torch.tensor([0.0, 0.0, 0.62])
         self._env_spacing = self._task_cfg["env"]["envSpacing"]
-        self._guarddog_positions = torch.tensor([0.0, 0.0, 2.0])
 
-        self._reset_dist = self._task_cfg["env"]["resetDist"]
-        self._max_push_effort = self._task_cfg["env"]["maxEffort"]
-
-    def set_up_scene(self, scene) -> None:
+    def set_up_scene(self, scene) -> None: 
         self.get_guarddog()
         super().set_up_scene(scene)
-        self._guarddogs = ArticulationView(
-            prim_paths_expr="/World/envs/.*/Guarddog/Body", name="guarddog_view", reset_xform_properties=False
+        self._guarddogs = GuarddogView(
+            prim_paths_expr="/World/envs/.*/Guarddog/Body", name="guarddog_view"
         )
         scene.add(self._guarddogs)
         return
@@ -74,33 +116,62 @@ class GuarddogTask(RLTask):
         super().initialize_views(scene)
         if scene.object_exists("guarddog_view"):
             scene.remove_object("guarddog_view", registry_only=True)
-        self._guarddogs = ArticulationView(
-            prim_paths_expr="/World/envs/.*/Guarddog", name="guarddog_view", reset_xform_properties=False
+        self._guarddogs = GuarddogView(
+            prim_paths_expr="/World/envs/.*/Guarddog/Body", name="guarddog_view"
         )
         scene.add(self._guarddogs)
 
     def get_guarddog(self):
         guarddog = Guarddog(
-            prim_path=self.default_zero_env_path + "/Guarddog", name="Guarddog", translation=self._guarddog_positions
+            prim_path=self.default_zero_env_path + "/Guarddog", name="Guarddog", translation=self._guarddog_translation
         )
-        # applies articulation settings from the task configuration yaml file
         self._sim_config.apply_articulation_settings(
             "Guarddog", get_prim_at_path(guarddog.prim_path), self._sim_config.parse_actor_config("Guarddog")
         )
+        print(guarddog.prim_path)
+        # Configure joint properties
+        joint_paths = []
+        for quadrant in ["FL", "BL", "FR", "BL"]:
+            for component, abbrev in [("Hip", "J2"), ("Thigh", "J3")]:
+                joint_paths.append(f"{quadrant}_{component}/{quadrant}_{abbrev}")
+            joint_paths.append(f"Body/{quadrant}_J1")
+        for joint_path in joint_paths:
+            # print(f"{guarddog.prim_path}/{joint_path}")
+            set_drive(f"{guarddog.prim_path}/{joint_path}", "angular", "position", 0, 400, 40, 2)
 
     def get_observations(self) -> dict:
-        # dof_pos = self._guarddogs.get_joint_positions(clone=False)
-        # dof_vel = self._guarddogs.get_joint_velocities(clone=False)
+        torso_position, torso_rotation = self._guarddogs.get_world_poses(clone=False)
+        root_velocities = self._guarddogs.get_velocities(clone=False)
+        dof_pos = self._guarddogs.get_joint_positions(clone=False)
+        dof_vel = self._guarddogs.get_joint_velocities(clone=False)
 
-        # self.cart_pos = dof_pos[:, self._cart_dof_idx]
-        # self.cart_vel = dof_vel[:, self._cart_dof_idx]
-        # self.pole_pos = dof_pos[:, self._pole_dof_idx]
-        # self.pole_vel = dof_vel[:, self._pole_dof_idx]
+        velocity = root_velocities[:, 0:3]
+        ang_velocity = root_velocities[:, 3:6]
 
-        # self.obs_buf[:, 0] = self.cart_pos
-        # self.obs_buf[:, 1] = self.cart_vel
-        # self.obs_buf[:, 2] = self.pole_pos
-        # self.obs_buf[:, 3] = self.pole_vel
+        base_lin_vel = quat_rotate_inverse(torso_rotation, velocity) * self.lin_vel_scale
+        base_ang_vel = quat_rotate_inverse(torso_rotation, ang_velocity) * self.ang_vel_scale
+        projected_gravity = quat_rotate(torso_rotation, self.gravity_vec)
+        dof_pos_scaled = (dof_pos - self.default_dof_pos) * self.dof_pos_scale
+
+        commands_scaled = self.commands * torch.tensor(
+            [self.lin_vel_scale, self.lin_vel_scale, self.ang_vel_scale],
+            requires_grad=False,
+            device=self.commands.device,
+        )
+
+        obs = torch.cat(
+            (
+                base_lin_vel,
+                base_ang_vel,
+                projected_gravity,
+                commands_scaled,
+                dof_pos_scaled,
+                dof_vel * self.dof_vel_scale,
+                self.actions,
+            ),
+            dim=-1,
+        )
+        self.obs_buf[:] = obs
 
         observations = {self._guarddogs.name: {"obs_buf": self.obs_buf}}
         return observations
@@ -109,59 +180,138 @@ class GuarddogTask(RLTask):
         if not self.world.is_playing():
             return
 
-        # reset_env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
-        # if len(reset_env_ids) > 0:
-        #     self.reset_idx(reset_env_ids)
+        reset_env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
+        if len(reset_env_ids) > 0:
+            self.reset_idx(reset_env_ids)
 
-        # actions = actions.to(self._device)
-
-        # forces = torch.zeros((self._guarddogs.count, self._guarddogs.num_dof), dtype=torch.float32, device=self._device)
-        # forces[:, self._cart_dof_idx] = self._max_push_effort * actions[:, 0]
-
-        # indices = torch.arange(self._guarddogs.count, dtype=torch.int32, device=self._device)
-        # self._guarddogs.set_joint_efforts(forces, indices=indices)
+        indices = torch.arange(self._guarddogs.count, dtype=torch.int32, device=self._device)
+        self.actions[:] = actions.clone().to(self._device)
+        current_targets = self.current_targets + self.action_scale * self.actions * self.dt
+        self.current_targets[:] = tensor_clamp(
+            current_targets, self.guarddog_dof_lower_limits, self.guarddog_dof_upper_limits
+        )
+        self._guarddogs.set_joint_position_targets(self.current_targets, indices)
 
     def reset_idx(self, env_ids):
-        # num_resets = len(env_ids)
+        num_resets = len(env_ids)
+        # randomize DOF velocities
+        velocities = torch_rand_float(-0.1, 0.1, (num_resets, self._guarddogs.num_dof), device=self._device)
+        dof_pos = self.default_dof_pos[env_ids]
+        dof_vel = velocities
 
-        # # randomize DOF positions
-        # dof_pos = torch.zeros((num_resets, self._guarddogs.num_dof), device=self._device)
-        # dof_pos[:, self._cart_dof_idx] = 1.0 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
-        # dof_pos[:, self._pole_dof_idx] = 0.125 * math.pi * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
+        self.current_targets[env_ids] = dof_pos[:]
 
-        # # randomize DOF velocities
-        # dof_vel = torch.zeros((num_resets, self._guarddogs.num_dof), device=self._device)
-        # dof_vel[:, self._cart_dof_idx] = 0.5 * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
-        # dof_vel[:, self._pole_dof_idx] = 0.25 * math.pi * (1.0 - 2.0 * torch.rand(num_resets, device=self._device))
+        root_vel = torch.zeros((num_resets, 6), device=self._device)
 
-        # # apply resets
-        # indices = env_ids.to(dtype=torch.int32)
-        # self._guarddogs.set_joint_positions(dof_pos, indices=indices)
-        # self._guarddogs.set_joint_velocities(dof_vel, indices=indices)
+        # apply resets
+        indices = env_ids.to(dtype=torch.int32)
+        self._guarddogs.set_joint_positions(dof_pos, indices)
+        self._guarddogs.set_joint_velocities(dof_vel, indices)
+
+        self._guarddogs.set_world_poses(
+            self.initial_root_pos[env_ids].clone(), self.initial_root_rot[env_ids].clone(), indices
+        )
+        self._guarddogs.set_velocities(root_vel, indices)
+
+        self.commands_x[env_ids] = torch_rand_float(
+            self.command_x_range[0], self.command_x_range[1], (num_resets, 1), device=self._device
+        ).squeeze()
+        self.commands_y[env_ids] = torch_rand_float(
+            self.command_y_range[0], self.command_y_range[1], (num_resets, 1), device=self._device
+        ).squeeze()
+        self.commands_yaw[env_ids] = torch_rand_float(
+            self.command_yaw_range[0], self.command_yaw_range[1], (num_resets, 1), device=self._device
+        ).squeeze()
 
         # bookkeeping
         self.reset_buf[env_ids] = 0
         self.progress_buf[env_ids] = 0
+        self.last_actions[env_ids] = 0.0
+        self.last_dof_vel[env_ids] = 0.0
 
     def post_reset(self):
-        pass
-        # self._cart_dof_idx = self._guarddogs.get_dof_index("cartJoint")
-        # self._pole_dof_idx = self._guarddogs.get_dof_index("poleJoint")
-        # # randomize all envs
-        # indices = torch.arange(self._guarddogs.count, dtype=torch.int64, device=self._device)
-        # self.reset_idx(indices)
+        self.default_dof_pos = torch.zeros(
+            (self.num_envs, 12), dtype=torch.float, device=self.device, requires_grad=False
+        )
+        dof_names = self._guarddogs.dof_names
+        for i in range(self.num_actions):
+            name = dof_names[i]
+            print('!!!')
+            print(self.named_default_joint_angles)
+            print(name)
+            angle = self.named_default_joint_angles[name]
+            self.default_dof_pos[:, i] = angle
+
+        self.initial_root_pos, self.initial_root_rot = self._guarddogs.get_world_poses()
+        self.current_targets = self.default_dof_pos.clone()
+
+        dof_limits = self._guarddogs.get_dof_limits()
+        self.guarddog_dof_lower_limits = dof_limits[0, :, 0].to(device=self._device)
+        self.guarddog_dof_upper_limits = dof_limits[0, :, 1].to(device=self._device)
+
+        self.commands = torch.zeros(self._num_envs, 3, dtype=torch.float, device=self._device, requires_grad=False)
+        self.commands_y = self.commands.view(self._num_envs, 3)[..., 1]
+        self.commands_x = self.commands.view(self._num_envs, 3)[..., 0]
+        self.commands_yaw = self.commands.view(self._num_envs, 3)[..., 2]
+
+        # initialize some data used later on
+        self.extras = {}
+        self.gravity_vec = torch.tensor([0.0, 0.0, -1.0], device=self._device).repeat((self._num_envs, 1))
+        self.actions = torch.zeros(
+            self._num_envs, self.num_actions, dtype=torch.float, device=self._device, requires_grad=False
+        )
+        self.last_dof_vel = torch.zeros(
+            (self._num_envs, 12), dtype=torch.float, device=self._device, requires_grad=False
+        )
+        self.last_actions = torch.zeros(
+            self._num_envs, self.num_actions, dtype=torch.float, device=self._device, requires_grad=False
+        )
+
+        self.time_out_buf = torch.zeros_like(self.reset_buf)
+
+        # randomize all envs
+        indices = torch.arange(self._guarddogs.count, dtype=torch.int64, device=self._device)
+        self.reset_idx(indices)
 
     def calculate_metrics(self) -> None:
         pass
-        # reward = 1.0 - self.pole_pos * self.pole_pos - 0.01 * torch.abs(self.cart_vel) - 0.005 * torch.abs(self.pole_vel)
-        # reward = torch.where(torch.abs(self.cart_pos) > self._reset_dist, torch.ones_like(reward) * -2.0, reward)
-        # reward = torch.where(torch.abs(self.pole_pos) > np.pi / 2, torch.ones_like(reward) * -2.0, reward)
+        torso_position, torso_rotation = self._guarddogs.get_world_poses(clone=False)
+        root_velocities = self._guarddogs.get_velocities(clone=False)
+        dof_pos = self._guarddogs.get_joint_positions(clone=False)
+        dof_vel = self._guarddogs.get_joint_velocities(clone=False)
 
-        # self.rew_buf[:] = reward
+        velocity = root_velocities[:, 0:3]
+        ang_velocity = root_velocities[:, 3:6]
+
+        base_lin_vel = quat_rotate_inverse(torso_rotation, velocity)
+        base_ang_vel = quat_rotate_inverse(torso_rotation, ang_velocity)
+
+        # velocity tracking reward
+        lin_vel_error = torch.sum(torch.square(self.commands[:, :2] - base_lin_vel[:, :2]), dim=1)
+        ang_vel_error = torch.square(self.commands[:, 2] - base_ang_vel[:, 2])
+        rew_lin_vel_xy = torch.exp(-lin_vel_error / 0.25) * self.rew_scales["lin_vel_xy"]
+        rew_ang_vel_z = torch.exp(-ang_vel_error / 0.25) * self.rew_scales["ang_vel_z"]
+
+        rew_lin_vel_z = torch.square(base_lin_vel[:, 2]) * self.rew_scales["lin_vel_z"]
+        rew_joint_acc = torch.sum(torch.square(self.last_dof_vel - dof_vel), dim=1) * self.rew_scales["joint_acc"]
+        rew_action_rate = (
+            torch.sum(torch.square(self.last_actions - self.actions), dim=1) * self.rew_scales["action_rate"]
+        )
+        rew_cosmetic = (
+            torch.sum(torch.abs(dof_pos[:, 0:4] - self.default_dof_pos[:, 0:4]), dim=1) * self.rew_scales["cosmetic"]
+        )
+
+        total_reward = rew_lin_vel_xy + rew_ang_vel_z + rew_joint_acc + rew_action_rate + rew_cosmetic + rew_lin_vel_z
+        total_reward = torch.clip(total_reward, 0.0, None)
+
+        self.last_actions[:] = self.actions[:]
+        self.last_dof_vel[:] = dof_vel[:]
+
+        self.fallen_over = self._guarddogs.is_base_below_threshold(threshold=0.3, ground_heights=0.0)
+        total_reward[torch.nonzero(self.fallen_over)] = -1
+        self.rew_buf[:] = total_reward.detach()
 
     def is_done(self) -> None:
-        pass
-        # resets = torch.where(torch.abs(self.cart_pos) > self._reset_dist, 1, 0)
-        # resets = torch.where(torch.abs(self.pole_pos) > math.pi / 2, 1, resets)
-        # resets = torch.where(self.progress_buf >= self._max_episode_length, 1, resets)
-        # self.reset_buf[:] = resets
+        # reset agents
+        time_out = self.progress_buf >= self.max_episode_length - 1
+        self.reset_buf[:] = time_out | self.fallen_over
